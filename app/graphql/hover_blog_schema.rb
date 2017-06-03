07c7548c6a722c7b6fd77bcb683b003bff3e5472
Jon Stokes <jon@jonstokes.com>
2017-06-03 17:04:00 -0500

    Added object fetch methods to schema

--- app/graphql/hover_blog_schema.rb
+++ app/graphql/hover_blog_schema.rb
@@ -1,3 +1,15 @@
 HoverBlogSchema = GraphQL::Schema.define do
   query(Types::QueryType)
+
+  id_from_object ->(object, type_definition, query_ctx) {
+    # Call your application's UUID method here
+    # It should return a string
+    GraphQL::Schema::UniqueWithinType.encode(type_definition.name, object.id)
+  }
+
+  object_from_id ->(id, query_ctx) {
+    type_name, item_id = GraphQL::Schema::UniqueWithinType.decode(id)
+
+    Database.send("get_#{type_name}", item_id)
+  }
 end
