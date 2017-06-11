0c2dd65c15c2d72169a2e9756c21b8bc198ccefa
Jon Stokes <jon@jonstokes.com>
2017-06-10 20:39:05 -0500

    Still trying to make this work

--- app/graphql/types/feature_type.rb
+++ app/graphql/types/feature_type.rb
@@ -3,9 +3,9 @@ Types::FeatureType = GraphQL::ObjectType.define do
   global_id_field :id
 
   field :id, !types.ID
-  field :name, !types.String
-  field :description, !types.String
-  field :url, !types.String
+  field :name, types.String
+  field :description, types.String
+  field :url, types.String
 
   implements GraphQL::Relay::Node.interface
 end
